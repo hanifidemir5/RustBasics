b43f8418e01eca56bf95a451f20310e2e50ebd58
hanifi5 <hanifidemir.sftwr@gmail.com>
2025-01-19 21:47:00 +0300

    Closures

--- src/closures.rs
+++ src/closures.rs
@@ -1,3 +1,5 @@
+use std::fmt::format;
+
 pub fn closures() {
     let my_closure = || println!("Defining closures");
 
@@ -5,6 +7,30 @@ pub fn closures() {
 
     let even_numbers = |x:i32| -> bool { x % 2 == 0};
 
-    even_numbers(4);
-    even_numbers(5); 
+    let even = even_numbers(4);
+    let odd = even_numbers(5); 
+
+    println!("Even :{}, Odd: {}", even, odd);
+
+    let numbers = vec![1,2,3,4];
+
+    let even_numbers:Vec<i32> = numbers.into_iter().filter(|x| *x % 2 == 0).collect();
+
+    println!("{:?}", even_numbers);
+
+    let print_data = |data: &str|{
+        println!("Received data: {}", data);
+    };
+
+    download_data("patika.dev", print_data);
+}
+
+fn download_data(url:&str, callback:impl FnOnce(&str)){
+    println!("Getting data from {}", url);
+
+    std::thread::sleep(std::time::Duration::from_secs(1));
+
+    let data = format!("Some data from {}", url);
+
+    callback(&data);
 }
